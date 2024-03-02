88291402b7daedd7adf81059dfa9351e2f3fe099
gerardjosraudale <114362615+gerardjosraudale@users.noreply.github.com>
2024-03-02 12:59:53 -0600

    Cambios 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Call DisplayWelcome function
+        DisplayWelcome();
+
+        // Call PromptUserName function and save the returned name
+        string userName = PromptUserName();
+
+        // Call PromptUserNumber function and save the returned number
+        int userNumber = PromptUserNumber();
+
+        // Call SquareNumber function passing the user's number and save the squared number
+        int squaredNumber = SquareNumber(userNumber);
+
+        // Call DisplayResult function passing the user's name and squared number
+        DisplayResult(userName, squaredNumber);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string userName, int squaredNumber)
+    {
+        Console.WriteLine($"Hello, {userName}! Your favorite number squared is: {squaredNumber}");
     }
 }
